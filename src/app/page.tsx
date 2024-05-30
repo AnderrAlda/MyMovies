42aeac34b1dade3a463eba2144d799f38e3c0954
Ander Aldabaldetreku <anderaldabaldetreku@gmail.com>
2024-05-30 14:15:41 +0200

    each user each data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,31 @@
 import { Console } from "console";
 import Link from "next/link";
-import { db } from "~/server/db";
 import { SignedIn, SignedOut } from '@clerk/nextjs'
-
-
+import { getMyImages } from "~/server/queries";
 export const dynamic = "force-dynamic";
 
 
 
 const Images = async () => {
-  const images = await db.query.images.findMany(
-    {
-      orderBy: (model, { desc }) => desc(model.id),
-    }
-  );
+
+
+  const images = await getMyImages();
+
 
   return (
-    <div>   {
-      images.map((image) => (
-        <div key={image.id} className="w-1/3 p-4">
-          <p className="text-black">{image.name}</p>
-          <img src={image.url} alt="image" />
+    <div>
 
-        </div>
-      ))
-    }</div>
+
+      {
+
+        images.map((image) => (
+          <div key={image.id} className="w-1/3 p-4">
+            <p className="text-black">{image.name}</p>
+            <img src={image.url} alt="image" />
+
+          </div>
+        ))
+      }</div>
   )
 }
 
