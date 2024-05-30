79b5cd7b268be1a13ad0a25ed72b89fd00dec5f2
Ander Aldabaldetreku <anderaldabaldetreku@gmail.com>
2024-05-30 10:22:30 +0200

    basic database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { Console } from "console";
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
 
   const temporalimages = [
     { id: 1, url: "https://utfs.io/f/2f8a26f4-58ff-40da-85d2-13cb9f39dfbd-1nq2cb.jpg" },
@@ -16,6 +22,13 @@ export default function HomePage() {
         temporalimages.map((image) => (
           <div key={image.id} className="w-1/3 p-4">
             <img src={image.url} alt="image" />
+          </div>
+        ))
+      }
+      {
+        posts.map((post) => (
+          <div key={post.id} className="w-1/3 p-4">
+            <p className="text-black">{post.name}</p>
           </div>
         ))
       }
