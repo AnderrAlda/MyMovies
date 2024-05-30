230b425a54bea5f0a17ff69bd72ab1334639aabf
Ander Aldabaldetreku <anderaldabaldetreku@gmail.com>
2024-05-30 10:27:23 +0200

    dynamic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Console } from "console";
 import Link from "next/link";
 import { db } from "~/server/db";
 
+
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
 
   const posts = await db.query.posts.findMany();
